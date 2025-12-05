992ef6b8c5f5e5bf9af024d29f3921c5d76e3f91
Nguyen Nguyen <nguyen@reverb.com>
2025-12-05 14:44:08 -0600

    add comments

--- k8x-sbx-install.sh
+++ k8x-sbx-install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+# Quick install for k8x and sbx on macOS
+
 curl -fsSL https://raw.githubusercontent.com/reverbdotcom/gists/refs/heads/main/k8x.Brewfile \
     | brew bundle install --file=- \
     && export GOPRIVATE=github.com/reverbdotcom/k8x \
